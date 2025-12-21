a12807cbef0084bd7a318e82b32ae984395390c7
mckim <mckim@stanford.edu>
2025-12-20 22:17:49 -0800

    Train model with device setting.

--- python/RLC/grid/grid.py
+++ python/RLC/grid/grid.py
@@ -7,7 +7,7 @@ class Grid():
 
 
     def board_to_tensor(self, board):
-        tensor = torch.zeros(1, 12, 8, 8)
+        tensor = torch.zeros(1, 12, 8, 8, device=self.config['device'])
         piece_map = {'P':0, 'N':1, 'B':2, 'R':3, 'Q':4, 'K':5, 'p':6, 'n':7, 'b':8, 'r':9, 'q':10, 'k':11}
         for sq, pc in board.piece_map().items():
             tensor[0, piece_map[pc.symbol()], sq // 8, sq % 8] = 1.0
